5e9bdcbf5e9b056a3b2cdfbfdec81ffa6d661843
lmdixon23 <lmdixon23@gmail.com>
2026-06-02 10:35:22 +0800

    Update conftest.py

--- machine_learning/image_captioning_cnn_rnn_tpu/conftest.py
+++ machine_learning/image_captioning_cnn_rnn_tpu/conftest.py
@@ -9,4 +9,8 @@ where the working directory differs.
 import os
 import sys
 
+# Route tf.keras to Keras 2 (tf-keras) so this project's tf.keras.preprocessing
+# APIs resolve on TensorFlow >= 2.16. Must be set before tensorflow is imported.
+os.environ.setdefault("TF_USE_LEGACY_KERAS", "1")
+
 sys.path.insert(0, os.path.dirname(__file__))
